21113915f095f5516a2a137d5d200ad0aa580ac4
adamwshero <adamwshero@gmail.com>
2022-09-26 17:57:07 -0600

    .

--- main.tf
+++ main.tf
@@ -122,47 +122,7 @@ resource "pagerduty_extension" "this" {
 
   name              = var.extension_name
   endpoint_url      = var.endpoint_url
-  extension_schema  = data.pagerduty_extension_schema.this[count.index].id
+  extension_schema  = data.pagerduty_extension_schema.this[0].id
   extension_objects = [pagerduty_service.this.id]
   config = var.config
-
-#   config = <<EOF
-#   {
-#       "app_id": "${var.app_id}",
-#       "authed_user": {
-#         "id": "${var.authed_user}"
-#       },
-#       "bot_user_id": "${var.bot_user_id}",
-#       "enterprise": "null",
-#       "incoming_webhook": {
-#         "channel": "${var.slack_channel}",
-#         "channel_id": "${var.slack_channel_id}",
-#         "configuration_url": "${var.configuration_url}",
-#         "url": "${var.url}"
-#       },
-#       "restrict": "any",
-#       "is_enterprise_install": "false",
-#       "notify_types": {
-#         "resolve": "${var.notify_resolve}",
-#         "trigger": "${var.notify_trigger}",
-#         "escalate": "${var.notify_escalate}",
-#         "acknowledge": "${var.notify_acknowledge}",
-#         "assignments": "${var.notify_assignments}",
-#         "annotate": "${var.notify_annotate}"
-#       },
-#       "ok": true,
-#       "referer": "${var.referer}",
-#       "scope": "app_mentions:read,channels:join,channels:manage,channels:read,chat:write,chat:write.public,commands,groups:read,groups:write,im:read,im:write,incoming-webhook,pins:write,team:read,users:read,workflow.steps:execute",
-#       "team": {
-#         "id": "${var.slack_team_id}",
-#         "name": "${var.slack_team_name}"
-#       },
-#       "token_type": "bot",
-#       "urgency": {
-#         "high": "${var.high_urgency}",
-#         "low": "${var.low_urgency}"
-#       }
-#     }
-# EOF
-
 }
