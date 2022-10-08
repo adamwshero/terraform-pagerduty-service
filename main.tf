1c5291aab4aad43cba6e2253e7b35f30bd8cb6be
adamwshero <adamwshero@gmail.com>
2022-10-07 20:18:04 -0600

    .

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "pagerduty_slack_connection" "this" {
       notification_type = type.notification_type
       config            = type.config
     }
-    if var.create_slack_connection == true ? [true] : []
+    if var.create_slack_connection == true
   }
   source_id         = each.value.source_id
   source_type       = each.value.source_type
