28bc8002f5997795beeda3eee2a2f4e8ab412220
adamwshero <adamwshero@gmail.com>
2022-10-07 18:05:40 -0600

    .

--- main.tf
+++ main.tf
@@ -119,9 +119,9 @@ resource "pagerduty_maintenance_window" "this" {
 }
 
 resource "pagerduty_slack_connection" "service_reference" {
-  for_each = var.source_type == "service_reference" ? [true] : []
+  for_each = var.source_type == "service_reference" ? [pagerduty_service.this.id] : [var.source_id]
 
-  source_id         = pagerduty_service.this
+  source_id         = pagerduty_service.this.id
   source_type       = var.source_type
   workspace_id      = var.workspace_id
   channel_id        = var.channel_id
