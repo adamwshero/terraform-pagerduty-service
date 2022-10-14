483587e48e4ef18c20bf82a9c73871e1e42367f8
adamwshero <adamwshero@gmail.com>
2022-10-14 17:27:54 -0600

    .

--- main.tf
+++ main.tf
@@ -128,6 +128,7 @@ resource "pagerduty_slack_connection" "this" {
   for_each = {
     for type in var.slack_connection : type.source_type => {
       source_id         = var.source_type != "team_reference" ? pagerduty_service.this.id : null
+      source_type       = type.source_type
       workspace_id      = type.workspace_id
       channel_id        = type.channel_id
       notification_type = type.notification_type
