19fc37217082132375bc82877a4f7546f47a1267
adamwshero <adamwshero@gmail.com>
2022-10-14 16:54:28 -0600

    using locals

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "pagerduty" {
 }
 
 locals {
-  source_id = pagerduty_service.this.id
+  service_reference = pagerduty_service.this.id
+  team_reference    = var.source_id
 }
 
 data "pagerduty_escalation_policy" "this" {
@@ -126,7 +127,7 @@ resource "pagerduty_maintenance_window" "this" {
 resource "pagerduty_slack_connection" "this" {
   for_each = {
     for type in var.slack_connection : type.source_type => {
-      source_id         = type.source_id
+      source_id         = var.source_type == local.team_reference ? var.source_id : pagerduty_service.this.id
       source_type       = type.source_type
       workspace_id      = type.workspace_id
       channel_id        = type.channel_id
