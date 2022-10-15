835af299f7bc3d6a8381f86ebfdd572689f9da95
adamwshero <adamwshero@gmail.com>
2022-10-14 22:48:27 -0600

    .

--- variables.tf
+++ variables.tf
@@ -45,16 +45,13 @@ variable "alert_creation" {
   description = "(Optional) Must be one of two values. PagerDuty receives events from your monitoring systems and can then create incidents in different ways. Value `create_incidents` is default: events will create an incident that cannot be merged. Value `create_alerts_and_incidents` is the alternative: events will create an alert and then add it to a new incident, these incidents can be merged. This option is recommended."
 }
 variable "incident_urgency_rule" {
-  type    = any
-  default = []
+  type = any
 }
 variable "support_hours" {
-  type    = any
-  default = []
+  type = any
 }
 variable "scheduled_actions" {
-  type    = any
-  default = []
+  type = any
 }
 
 #################################
